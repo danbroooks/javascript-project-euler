dcb6a511160daae88bdea6df59eb40e48a82a323
Dan Brooks <dmbrooks@live.co.uk>
2016-11-23 10:39:11 +0000

    Memoize isPalindrome

--- util/isPalindrome.js
+++ util/isPalindrome.js
@@ -1,10 +1,11 @@
 'use strict'
 
 let reverse = require('./reverse')
+let memoize = require('./memoize')
 
 let splitNum = n => (n + '').split('').map(i => i / 1)
 
-module.exports = n => {
+module.exports = memoize(n => {
   let split = splitNum(n)
   return split.join('') == reverse(split).join('')
-}
+})
