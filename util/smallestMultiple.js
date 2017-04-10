d7faec73c41358b6ced193ab2fb08302b9e7f466
Dan Brooks <dmbrooks@live.co.uk>
2017-04-10 21:37:28 +0100

    Correct misleading variable name

--- util/smallestMultiple.js
+++ util/smallestMultiple.js
@@ -4,14 +4,14 @@ let range = require('./range')
 let isPrime = require('./isPrime')
 let memoize = require('./memoize')
 
-let smallestMultiple = memoize(power => {
-  if (power < 2) {
+let smallestMultiple = memoize(upto => {
+  if (upto < 2) {
     return 1
   }
 
-  let numbers = range(power).map(n => n + 1)
+  let numbers = range(upto).map(n => n + 1)
   let inc = numbers.filter(isPrime).reduce((a, b) => a * b)
-  let i = Math.floor(smallestMultiple(power - 1) / inc) * inc
+  let i = Math.floor(smallestMultiple(upto - 1) / inc) * inc
   let smallest = undefined
 
   while (!smallest) {
